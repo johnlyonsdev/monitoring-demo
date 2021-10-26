78807248c7a622536694174a91129bfec77c475f
John Lyons <johnlyonsdev@gmail.com>
2021-10-26 13:04:17 -0500

    added functionality

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path')
 const Rollbar = require('rollbar')
 
 let rollbar = new Rollbar({
-    accessToken: '6163b142150d4020b0853512bba40627',
+    accessToken: 'd84a4c049e4d45699f1e7005e55873cb',
     captureUncaught: true,
     captureUnhandledRejections: true
 })
@@ -15,6 +15,20 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
     rollbar.info('html file served successfully')
 })
+
+let students = []
+
+app.post('/api/student', (req, res) => {
+    const {name} = req.body
+    name = name.trim()
+
+    students.push(name)
+    rollbar.log('Student Added successfully', {
+        author: 'John', type: 'manual entry'
+    })
+    res.status(200).send(students)
+})
+app.use(rollbar.errorHandler())
 
 const port = process.env.PORT || 4545
 
